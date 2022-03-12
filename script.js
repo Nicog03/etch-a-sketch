134126ce0a2dca23436eb49475c20e4774f519a6
nicolas <nicolas.lima03@hotmail.com>
2022-03-12 17:20:41 -0300

    added a paintPixel function

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ const grid = document.getElementById('grid');
 
 let size = 16; //it's the height and width of the grid;
 
+let mouseDown = false;
+window.onmousedown = () => mouseDown = true;
+window.onmouseup = () => mouseDown = false;
+
 // create grid:
 for (let a = 0; a < size * size; a++) {
     let pixel = document.createElement('div');
     pixel.classList.add('pixel');
+    pixel.addEventListener('mousedown', paintPixelOnClick);
+    pixel.addEventListener('mouseover', paintPixel);
     grid.appendChild(pixel);
+}
+
+function paintPixelOnClick() {
+    this.style.background = 'black';
+}
+
+function paintPixel(e) {
+    if (e.type == 'mouseover' && mouseDown) {
+        this.style.background = 'black';
+    }
 }
