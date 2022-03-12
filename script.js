5d9c40ad7509a841f599f04d162eb95062c79411
nicolas <nicolas.lima03@hotmail.com>
2022-03-12 17:48:28 -0300

    added an eraser function

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ window.onmouseup = () => mouseDown = false;
 for (let a = 0; a < size * size; a++) {
     let pixel = document.createElement('div');
     pixel.classList.add('pixel');
+    pixel.setAttribute('draggable', false);
     pixel.addEventListener('mousedown', paintPixelOnClick);
     pixel.addEventListener('mouseover', paintPixel);
     grid.appendChild(pixel);
@@ -22,5 +23,42 @@ function paintPixelOnClick() {
 function paintPixel(e) {
     if (e.type == 'mouseover' && mouseDown) {
         this.style.background = 'black';
+    }
+}
+
+//=============================================================================
+//==============================ERASER SECTION=================================
+//=============================================================================
+
+let eraserButton = document.getElementById('eraser-button')
+
+eraserButtonIsActive = false;
+
+eraserButton.addEventListener('click', activateEraser)
+
+function activateEraser() {
+    if (!eraserButtonIsActive) {
+        eraserButtonIsActive = true;
+        eraserButton.classList.add('selected');
+
+        let pixels = document.querySelectorAll('.pixel')
+        pixels.forEach(pixel => pixel.addEventListener('mousedown', eraseOnClick));
+        pixels.forEach(pixel => pixel.addEventListener('mouseover', erase));
+    } else if (eraserButtonIsActive) {
+        eraserButtonIsActive = false;
+        eraserButton.classList.remove('selected');
+
+        let pixels = document.querySelectorAll('.pixel')
+        pixels.forEach(pixel => pixel.removeEventListener('mousedown', eraseOnClick));
+        pixels.forEach(pixel => pixel.removeEventListener('mouseover', erase));
+    }
+}
+
+function eraseOnClick() {
+    this.style.background = '#e6e6e6';
+}
+function erase(e) {
+    if (e.type == 'mouseover' && mouseDown) {
+        this.style.background = '#e6e6e6';
     }
 }
